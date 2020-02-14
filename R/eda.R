b563bbb3b3fe176441f50c052f50742ab36962ee
Amrit Singh <asingh.analytics@gmail.com>
2020-02-14 18:06:17 -0500

    add eda function

---
+++ R/eda.R
@@ -0,0 +1,11 @@
+#' eda
+#'
+#' build various biomarker panels
+#' @param eset list of nxp matrices (expression sets)
+#' @param response variable of any type (optional)
+#' @param demo - demographics data
+#' @param ncomp number of principal components to retain
+#' @export
+eda = function(eset, response=NULL, demo=NULL, ncomp = 2){
+  return(1)
+}
