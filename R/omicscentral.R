c353c91810e044703db908645930b41d19853ddf
Amrit Singh <asingh.analytics@gmail.com>
2020-02-21 21:04:23 -0500

    change CRAN to local

--- R/omicscentral.R
+++ R/omicscentral.R
@@ -4,7 +4,7 @@
 
 release_bullets <- function() {
   c(
-    '`usethis::use_latest_dependencies(TRUE, "CRAN")`',
+    '`usethis::use_latest_dependencies(TRUE, "local")`',
     '`omicscentral_dependency_dissuade()`'
   )
 }
